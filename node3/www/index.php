5bbae509ba37cdc53d11f6e74e2d7a193d647520
gunturmisbahsedjati <gunturmisbahsedjati@gmail.com>
2025-02-15 01:43:38 +0700

    first commit

---
+++ node3/www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Server Docker Ku 😁</title>
+</head>
+
+<body>
+  <h1 align="center">Hi There ! 👋<br>NODE 3</h1>
+</body>
+
+</html>
